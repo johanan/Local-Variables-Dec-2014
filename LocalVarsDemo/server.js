1caabe69ac035f8ede9fc1e11355d8849d08e03f
Joshua Johanan <josh359@gmail.com>
2014-12-06 13:45:58 -0500

    messy server.js

--- LocalVarsDemo/server.js
+++ LocalVarsDemo/server.js
@@ -1,13 +1,79 @@
-﻿var express = require('express'),
-    app = express();
+﻿var http = require('http'),
+    fs = require('fs');
+
 var port = process.env.port || 1337;
 
-//serve static assets
-app.use(express.static(__dirname + '/static'));
+var app = new SimpleApp();
 
-//map functions to routes
-app.get('/express', function (req, res) {
-    res.send('Hey from Express!')
+app.use(undefined, staticFilesSetup(__dirname + '/static'));
+app.use(undefined, function (req, res, next) {
+    res.write('HEY from SimpleApp');
+    res.end();
 });
 
-var server = app.listen(port);
+var server = http.createServer();
+server.on('request', app.handle);
+server.listen(port);
+
+function SimpleApp() {
+    mwStack = [];
+    
+    this.use = function (route, func) {
+        mwStack.push({ route: route, func: func });
+    };
+    
+    this.routeMatch = function routeMatch(route, url) {
+        return route === undefined || route === url;
+    };
+    
+    this.handle = function (req, res) {
+        var index = 0,
+            routeMatch = this.routeMatch;
+        
+        var next = function next(err) {
+            var mw = mwStack[index]
+            index++;
+            
+            if (mw === undefined) {
+                return;
+            }
+            
+            if (routeMatch(mw.route, req.url)) {
+                if (mw.func.length === 3 && err === undefined)
+                    mw.func.call(this, req, res, next);
+                else if (mw.func.length === 4)
+                    mw.func.call(this, err, req, res, next);
+                else
+                    next(err);
+            } else {
+                next(err);
+            }
+        };
+        
+        next();
+    }.bind(this);
+    
+    return this;
+};
+
+function staticFilesSetup(serverPath) {
+    
+    return function staticFiles(req, res, next) {
+        var path = serverPath + req.url;
+        fs.stat(path, function (err, stats) {
+            if (err)
+                next();
+            
+            if (stats !== undefined && stats.isFile()) {
+                res.writeHeader(200, {
+                    'Content-Type': 'text/html',
+                    'Content-Length': stats.size
+                });
+                var readStream = fs.createReadStream(path);
+                readStream.pipe(res);
+            } else {
+                next();
+            }
+        });
+    };
+};
