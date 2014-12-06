d7325670ebc54bc569e1023b7e8087c67362bf63
Joshua Johanan <josh359@gmail.com>
2014-12-06 14:03:24 -0500

    now its a module

--- LocalVarsDemo/server.js
+++ LocalVarsDemo/server.js
@@ -1,79 +1,47 @@
 ﻿var http = require('http'),
-    fs = require('fs');
+    SimpleApp = require('SimpleApp'),
+    app = new SimpleApp();
 
 var port = process.env.port || 1337;
 
-var app = new SimpleApp();
-
-app.use(undefined, staticFilesSetup(__dirname + '/static'));
-app.use(undefined, function (req, res, next) {
-    res.write('HEY from SimpleApp');
+//route functions
+function test(req, res, next) {
+    res.write('Hey this is a test!');
     res.end();
-});
+};
 
+function log(req, res, next) {
+    console.log(req.url);
+    next();
+};
+
+function error(req, res, next) {
+    next(new Error('This is an error!'));
+};
+
+function notFound(req, res, next) {
+    res.statusCode = 404;
+    res.write('Not Found');
+    res.end();
+};
+
+function handleError(err, req, res, next) {
+    console.log(err);
+    res.statusCode = 500;
+    res.write('There was an error');
+    res.end();
+};
+
+//wiring up middleware
+app.use(undefined, SimpleApp.powered);
+app.use(undefined, log);
+app.use(undefined, SimpleApp.staticFiles(__dirname + '/static/'));
+app.use('/error', error);
+app.use('/test', test);
+app.use(undefined, notFound);
+app.use(undefined, handleError);
+
+//serving
 var server = http.createServer();
 server.on('request', app.handle);
 server.listen(port);
-
-function SimpleApp() {
-    mwStack = [];
-    
-    this.use = function (route, func) {
-        mwStack.push({ route: route, func: func });
-    };
-    
-    this.routeMatch = function routeMatch(route, url) {
-        return route === undefined || route === url;
-    };
-    
-    this.handle = function (req, res) {
-        var index = 0,
-            routeMatch = this.routeMatch;
-        
-        var next = function next(err) {
-            var mw = mwStack[index]
-            index++;
-            
-            if (mw === undefined) {
-                return;
-            }
-            
-            if (routeMatch(mw.route, req.url)) {
-                if (mw.func.length === 3 && err === undefined)
-                    mw.func.call(this, req, res, next);
-                else if (mw.func.length === 4)
-                    mw.func.call(this, err, req, res, next);
-                else
-                    next(err);
-            } else {
-                next(err);
-            }
-        };
-        
-        next();
-    }.bind(this);
-    
-    return this;
-};
-
-function staticFilesSetup(serverPath) {
-    
-    return function staticFiles(req, res, next) {
-        var path = serverPath + req.url;
-        fs.stat(path, function (err, stats) {
-            if (err)
-                next();
-            
-            if (stats !== undefined && stats.isFile()) {
-                res.writeHeader(200, {
-                    'Content-Type': 'text/html',
-                    'Content-Length': stats.size
-                });
-                var readStream = fs.createReadStream(path);
-                readStream.pipe(res);
-            } else {
-                next();
-            }
-        });
-    };
-};
